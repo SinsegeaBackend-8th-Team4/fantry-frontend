0cd729abe3b8a63be6721ef47793c33d961958d2
rekindle402 <rekindle402@gmail.com>
2025-09-17 09:01:56 +0900

    FIx : Syntax Error

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <script setup>
-<<<<<<< HEAD
 import { useUiStore } from '@/stores/uiStore';
 import LoadingSpinner from '@/components/common/atoms/LoadingSpinner.vue';
 
@@ -11,11 +10,6 @@ const uiStore = useUiStore();
   <LoadingSpinner v-if="uiStore.isLoading" />
   
   <!-- 라우터 뷰: 현재 경로에 맞는 페이지 컴포넌트가 렌더링됩니다. -->
-=======
-</script>
-
-<template>
->>>>>>> 2a291c3e0585b049f2d2fa5ef26047dbcabc06d9
   <router-view />
 </template>
 
