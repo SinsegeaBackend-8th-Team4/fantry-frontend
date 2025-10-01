14ad9635f3bf86f1f39ee68817e3320604f0e3c5
hye000ne <_hye_one@hanmail.net>
2025-10-01 11:16:52 +0900

    fix: 로컬 설정 수정

--- vite.config.js
+++ vite.config.js
@@ -6,26 +6,21 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      },
-    },
-  },
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
   css: {
     preprocessorOptions: {
       scss: {
         // node_modules에서 발생하는 Sass 경고를 무시하도록 설정
-        quietDeps: true,
-      },
-    },
-  },
+        quietDeps: true
+      }
+    }
+  }
 })
