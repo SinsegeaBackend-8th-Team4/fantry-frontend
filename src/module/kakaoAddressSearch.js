d0c2e499b7b2f4393bd0b1cd6fd0742eaea21d73
Choo-Choo-Moon <m34821@gmail.com>
2025-10-09 14:12:26 +0900

    kakao 주소검색 주석 및callback 타입체크, autoMapping 추가

--- src/module/kakaoAddressSearch.js
+++ src/module/kakaoAddressSearch.js
@@ -1,9 +1,17 @@
+/**
+ * 카카오(Daum) 주소 검색 API 래퍼
+ * @param {Function} callback - 주소 선택 시 실행될 콜백 함수
+ * @returns {void}
+ */
 const openAddressSearch = (callback) => {
   new daum.Postcode({
     oncomplete: function (data) {
-      callback(data)
+      if(typeof callback === 'function'){
+        callback(data)
+      }
     },
     focusInput: true,
+    autoMapping: true,
   }).open({
     popupTitle: '주소검색',
     popupKey: 'fantry_address_search',
