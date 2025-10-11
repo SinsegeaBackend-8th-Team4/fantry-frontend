887744b177233694d763a8f6380416f317d62686
Choo-Choo-Moon <m34821@gmail.com>
2025-10-11 21:35:11 +0900

    fix/payment payment api 추가 및 paymentModule 수정

---
+++ src/api/payment.js
@@ -0,0 +1,29 @@
+import { apiClient } from '.'
+
+export const requestPaymentCreate = (payload) => {
+  return apiClient.post('/payments', payload)
+}
+
+export const requestPaymentApprove = (orderId, payload) => {
+  return apiClient.post('/payments/' + orderId + '/approve', payload)
+}
+
+export const requestPaymentCancel = (orderId, payload) => {
+  return apiClient.post('/payments/' + orderId + '/cancel', payload)
+}
+
+export const requestPaymentApproveVerify = (orderId, payload) => {
+  return requestPaymentVerify(orderId, payload)
+}
+
+export const requestPaymentReturnVerify = (orderId, payload) => {
+  return requestPaymentVerify(orderId, payload)
+}
+
+export const requestPaymentCancelVerify = (orderId, payload) => {
+  return requestPaymentVerify(orderId, payload)
+}
+
+const requestPaymentVerify = (orderId, payload) => {
+  return apiClient.post('/payments/' + orderId + '/verify', payload)
+}
