83244178c95d109e97b65824f9516fcd61a752c2
Kim-sohee <qpdlql013@naver.com>
2025-10-15 16:24:11 +0900

    로그인, 회원가입 admin/user 분기 처리

--- src/pages/user/access/LoginPage.vue
+++ src/pages/user/access/LoginPage.vue
@@ -21,6 +21,7 @@
       userStore.setLoginInfo(response.data.tokenMemberResponse, response.data.accessToken);
       router.push('/');
     } catch (error) {
+      alert(error.response.data.message);
       console.log("로그인결과: ", error);
       router.push('/login/fail');
     }
@@ -279,10 +280,10 @@
   }
 
   .sns-btn.naver {
-    background-image: url("../../public/images/login_naver_btn.png");
+    background-image: url("/images/login_naver_btn.png");
   }
 
   .sns-btn.kakao {
-    background-image: url("../../public/images/login_kakao_btn.png");
+    background-image: url("/images/login_kakao_btn.png");
   }
 </style>
